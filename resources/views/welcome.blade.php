6e32fa23024e1f81bb6fa367983da24b2948d7a2
JoaoMesquitaL <joao.pedroml@protonmail.com>
2023-11-17 21:10:16 -0300

    Alinhamente de código Josefa e Joao e Contato SMTP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,7 +78,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/admin/dashboard') }}">Dashboard</a>
+                        <a href="{{ url('/dashboard') }}">Dashboard</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
