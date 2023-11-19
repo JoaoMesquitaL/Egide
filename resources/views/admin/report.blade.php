4bcc368bac574bc780204848113fd03e23e9db62
JoaoMesquitaL <joao.pedroml@protonmail.com>
2023-11-18 23:29:42 -0300

    novo plot de grafico line e ajuste de row section

--- resources/views/admin/report.blade.php
+++ resources/views/admin/report.blade.php
@@ -74,10 +74,8 @@
       <div class="grafico card z-depth-4">
           <h5 class="center"> Aquisição de usuários</h5>
             <canvas id="myChart" width="400" height="150"></canvas>
-            <h5>{{$products}}</h5>
             <script>
               document.addEventListener('DOMContentLoaded', function () {
-
               const ctx = document.getElementById('myChart');
               const myChart = new Chart(ctx, {
                   type: 'bar',
@@ -109,14 +107,49 @@
           }, true);
             </script>
       </div>           
-    </section> 
-    
-    <section class="graficos col s12 m6">            
-        <div class="grafico card z-depth-4">
-            <h5 class="center"> Produtos </h5>
-        <canvas id="myChart2" width="400" height="200"></canvas> 
-    </div>            
-   </section>             
+    </section>       
+</div>
+
+
+  <div class="row container ">
+    <section class="graficos col s12 m6" >            
+      <div class="grafico card z-depth-4">
+          <h5 class="center"> Aquisição de usuários</h5>
+            <canvas id="myChart2" width="400" height="150"></canvas>
+            <script>
+              document.addEventListener('DOMContentLoaded', function () {
+              const ctx = document.getElementById('myChart2');
+              const myChart2 = new Chart(ctx, {
+                  type: 'line',
+                  data: {
+                      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                      datasets: [{
+                          label: 'n° of Votes',
+                          data: [65, 59, 80, 81, 56, 55, 40],
+                          backgroundColor: [
+                              'rgba(255, 99, 132, 0.2)',
+                              'rgba(54, 162, 235, 0.2)',
+                              'rgba(255, 206, 86, 0.2)',
+                              'rgba(75, 192, 192, 0.2)',
+                              'rgba(153, 102, 255, 0.2)',
+                              'rgba(255, 159, 64, 0.2)'
+                          ],
+                          borderColor: [
+                              'rgba(255, 99, 132, 1)',
+                              'rgba(54, 162, 235, 1)',
+                              'rgba(255, 206, 86, 1)',
+                              'rgba(75, 192, 192, 1)',
+                              'rgba(153, 102, 255, 1)',
+                              'rgba(255, 159, 64, 1)'
+                          ],
+                          borderWidth: 1
+                      }]
+              }
+              });
+          }, true);
+            </script>
+      </div>           
+    </section>       
 </div>
 
 
